67cbf972c36c4ec9cb0a686ba026a5f37cce58ec
Damien Richcreek <damien@richcreek.dev>
2022-08-30 18:02:33 -0700

    added product form ui and options/variations

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,6 @@ import ProductList from "../components/ProductList";
 import styles from "../styles/Home.module.css";
 
 export default function Home({ products }) {
-  console.log(products);
   return (
     <>
       <ProductList products={products} />
