9b6395e828bb76840ccacb3139c265a8c8953972
Damien Richcreek <damien@richcreek.dev>
2022-09-01 07:00:41 -0700

    added recommended and footer components

--- components/Layout.js
+++ components/Layout.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Footer from "./Footer";
 import Nav from "./Nav";
 
 function Layout({ children }) {
@@ -6,7 +7,7 @@ function Layout({ children }) {
     <div className="flex flex-col justify-between min-h-screen">
       <Nav />
       <main>{children}</main>
-      <footer>Footer</footer>
+      <Footer />
     </div>
   );
 }
