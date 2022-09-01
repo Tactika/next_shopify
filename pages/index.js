cf206ebd4e5239e8ac6241cf75480ce107ecfadc
Damien Richcreek <damien@richcreek.dev>
2022-09-01 09:22:49 -0700

    updated seo to include facebook recommendations

--- pages/index.js
+++ pages/index.js
@@ -2,10 +2,24 @@ import { getProductsInCollection } from "../lib/shopify";
 import ProductList from "../components/ProductList";
 import styles from "../styles/Home.module.css";
 import Hero from "../components/Hero";
+import Head from "next/head";
 
 export default function Home({ products }) {
   return (
     <div className="">
+      <Head>
+        <title>Shopify eCommerce Store</title>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1"/>
+        <meta name="description" content="Modern eCommerce Store for purchasing heavy equipment, built using Next.js, Shopify, and TailwindCSS."/>
+        <meta property="og:title" content="Shopify eCommerce Store" />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://www.myroute.dev" />
+        <meta property="og:image" content="https://www.myroute.dev/share.png" />
+        <meta property="og:description" content="Modern eCommerce Store for purchasing heavy equipment, built using Next.js, Shopify, and TailwindCSS. Additonal topics include Storefront API, Static Site Generation, getStaticPaths, getStaticProps, and much much more!" />
+        <meta property="og:locale" content="en_US" />
+        <meta property="og:site_name" content="Shopify eCommerce Store" />
+      </Head>
       <Hero />
       <ProductList products={products} />
     </div>
