83ba48858b24566398bdde9de8b1e22f34d9ca28
Damien Richcreek <damien@richcreek.dev>
2022-09-01 07:23:11 -0700

    added hero section

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,14 @@
 import { getProductsInCollection } from "../lib/shopify";
 import ProductList from "../components/ProductList";
 import styles from "../styles/Home.module.css";
+import Hero from "../components/Hero";
 
 export default function Home({ products }) {
   return (
-    <>
+    <div className="">
+      <Hero />
       <ProductList products={products} />
-    </>
+    </div>
   );
 }
 
