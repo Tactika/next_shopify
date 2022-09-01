d3b3ec745d40b2c7f48c7875fcc63309723a4dd4
Damien Richcreek <damien@richcreek.dev>
2022-09-01 06:05:42 -0700

    added image carousel using swiperjs

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,10 @@
 import "../styles/globals.css";
+// import "swiper/swiper.scss";
+// import "swiper/components/navigation/navigation.scss";
+// import "swiper/components/pagination/pagination.scss";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
 import Layout from "../components/Layout";
 import ShopProvider from "../context/shopContext";
 import { useRouter } from "next/router";
